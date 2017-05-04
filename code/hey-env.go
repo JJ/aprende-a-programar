4c91f25721ea9c990874a1bef7168f323a134807
JJ Merelo <jjmerelo@gmail.com>
2017-05-04 13:25:59 +0200

    Añade nuevo programa

---
+++ code/hey-env.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/",
+		func (res http.ResponseWriter,
+			req *http.Request) {
+			fmt.Fprintln( res, `<html>
+  <head><title>Página</title></head>
+  <body><h1>Cargada</h1></body>
+</html>`)
+		})
+	ε := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if ε != nil {
+		panic( ε )
+	}
+}
+
